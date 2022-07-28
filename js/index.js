fdef1f35f0e258af91b518b98e6f8744753c35f6
jashamid <jasmin.ahamid@gmail.com>
2022-07-28 17:54:06 -0400

    Incorporate Javascript

---
+++ js/index.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+    $('.loader_bg').fadeToggle();
+}, 1500);
+
+$("button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".heading").offset().top},
+        'slow');
+});
