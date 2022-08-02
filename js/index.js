22b68dbe40ff5d3afdd25d2f99f73a4730584e9a
jashamid <jasmin.ahamid@gmail.com>
2022-08-02 17:34:30 -0400

    ux-19
    add java script + j query

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-// setTimeout(function(){
-//     $('.loader_bg').fadeToggle();
-// }, 1500);
+setTimeout(function(){
+    $('.loader_bg').fadeToggle();
+}, 1500);
 
-// $("button").click(function() {
-//     $('html,body').animate({
-//         scrollTop: $(".heading").offset().top},
-//         'slow');
-// });
+$("button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".heading").offset().top},
+        'slow');
+});
 
 console.log('index.jsloaded');
